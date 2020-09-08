46d50467d2aaf442c2de5dd284853f123b526ea7
gosuhiman <gosuhiman@gmail.com>
2020-09-09 00:00:04 +0200

    Extract boxes

--- src/main.py
+++ src/main.py
@@ -1,14 +1,24 @@
 import xml.etree.ElementTree as ElementTree
 
-filename = 'data/kangaroo/annots/00001.xml'
+test_filename = 'data/kangaroo/annots/00001.xml'
 
-tree = ElementTree.parse(filename)
-root = tree.getroot()
-print(root)
 
-for box in root.findall('.//bndbox'):
-    xmin = int(box.find('xmin').text)
-    ymin = int(box.find('ymin').text)
-    xmax = int(box.find('xmax').text)
-    ymax = int(box.find('ymax').text)
-    coors = [xmin, ymin, xmax, ymax]
+def extract_boxes(filename):
+    tree = ElementTree.parse(filename)
+    root = tree.getroot()
+    boxes = list()
+    for box in root.findall('.//bndbox'):
+        xmin = int(box.find('xmin').text)
+        ymin = int(box.find('ymin').text)
+        xmax = int(box.find('xmax').text)
+        ymax = int(box.find('ymax').text)
+        coors = [xmin, ymin, xmax, ymax]
+        boxes.append(coors)
+    width = int(root.find('.//size/width').text)
+    height = int(root.find('.//size/height').text)
+    return boxes, width, height
+
+
+boxes, width, height = extract_boxes(test_filename)
+print(boxes)
+print(width, height)
